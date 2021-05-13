c85abb4f18d71212afe51424e3cb1f065490a79f
AshishPayU <AshishPayU@users.noreply.github.com>
2021-05-13 18:32:45 +0530

    Update hasher.rb

--- lib/payu/hasher.rb
+++ lib/payu/hasher.rb
@@ -44,6 +44,18 @@ module PayU
         status:,
         additional_charges: ''
       )
+        
+=begin
+       Response received from Payment Gateway at this page.
+It is absolutely mandatory that the hash (or checksum) is computed again after you receive response from PayU and compare it with request and post back parameters. This will protect you from any tampering by the user and help in ensuring a safe and secure transaction experience. It is mandate that you secure your integration with PayU by implementing Verify webservice and Webhook/callback as a secondary confirmation of transaction response.
+
+Hash string without Additional Charges -
+hash = sha512(SALT|status||||||udf5|||||email|firstname|productinfo|amount|txnid|key)
+
+With additional charges - 
+hash = sha512(additionalCharges|SALT|status||||||udf5|||||email|firstname|productinfo|amount|txnid|key)
+=end
+        
       PayU::Validator.validate_hash_params({
         txn_id: txn_id,
         amount: amount,
